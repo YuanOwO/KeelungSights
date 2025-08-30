d1cf99e3579fc831029900818baa99cb2cb8a06d
YuanOwO <yuan@yuanowo.xyz>
2025-08-31 01:12:40 +0800

    爬蟲重構

---
+++ src/main/java/xyz/yuanowo/keelungsightsviewer/crawler/CrawlerUtils.kt
@@ -0,0 +1,76 @@
+package xyz.yuanowo.keelungsightsviewer.crawler
+
+import org.jsoup.nodes.Element
+import org.jsoup.nodes.TextNode
+
+
+class CrawlerUtils {
+
+    companion object {
+
+        fun hello() {
+            println("Hello, CrawlerUtils!")
+        }
+
+        fun getIDFromURL(url: String): Int? {
+            val matchResult = Regex("/tourguide/scenery(\\d+)\\.html").find(url)
+            return matchResult?.groups?.get(1)?.value?.toIntOrNull()
+        }
+
+        fun appendLF(sb: StringBuilder) {
+//         換到新的一行，但如果已在行首則不換行
+            if (sb.isNotEmpty() && sb.last() != '\n') {
+                sb.append('\n')
+            }
+        }
+
+        fun getTextFromElement(element: Element?): String? {
+            if (element == null)
+                return null
+
+            return buildString {
+                for (e in element.childNodes()) {
+                    when (e) {
+                        is TextNode -> {
+                            append(e.text().trim())
+                        }
+
+                        is Element -> {
+                            val text = e.text().trim()
+                            if (e.className() == "author" || e.className() == "clear" || e.id() == "po") {
+                                continue
+                            } else if (e.className() == "othermsg" || e.className() == "othermsg2") {
+                                appendLF(this)
+                                append(getTextFromElement(e))
+                            } else {
+                                when (e.tagName()) {
+                                    "a" -> append(text)
+                                    "strong" -> append("**$text**")
+                                    "br" -> append("\n")
+                                    "h4" -> {
+                                        appendLF(this)
+                                        append("#### $text\n")
+                                    }
+
+                                    "p" -> {
+                                        appendLF(this)
+                                        append(text)
+                                        appendLF(this)
+                                    }
+
+                                    else -> println("Unknown element: $e")
+                                }
+                            }
+                        }
+
+                        else -> {
+                            println("Unknown node: $e")
+                        }
+                    }
+                }
+            }.trim()
+        }
+
+    }
+
+}
--- src/main/java/xyz/yuanowo/keelungsightsviewer/service/SightsService.java
+++ src/main/java/xyz/yuanowo/keelungsightsviewer/service/SightsService.java
@@ -2,35 +2,31 @@ package xyz.yuanowo.keelungsightsviewer.service;
 
 import org.springframework.stereotype.Service;
 import xyz.yuanowo.keelungsightsviewer.crawler.SightsCrawler;
-import xyz.yuanowo.keelungsightsviewer.exception.InternalServerError;
 import xyz.yuanowo.keelungsightsviewer.exception.NotFoundException;
 import xyz.yuanowo.keelungsightsviewer.model.Sight;
 
-import java.io.IOException;
 import java.util.List;
 
 
 @Service
 public class SightsService {
 
+
     public List<Sight> getSightsByDistrict(String district) {
         SightsCrawler crawler = new SightsCrawler();
-
-        try {
-            return crawler.getSightsList(district);
-        } catch (IOException e) {
-            throw new InternalServerError("未知的錯誤: " + e.getMessage());
-        }
+        return crawler.fetchSightsListSync(district);
     }
 
     public Sight getSightById(Long id) {
         SightsCrawler crawler = new SightsCrawler();
+        Sight sight = crawler.fetchSightDetailsSync(String.format("/tourguide/scenery%d.html", id));
 
-        try {
-            return crawler.getSightDetails(String.format("/tourguide/scenery%d.html", id));
-        } catch (IOException e) {
+//     找不到或不在基隆市
+        if (sight == null || sight.getCity() == null || !sight.getCity().equals("基隆市")) {
             throw new NotFoundException("未知的景點 ID: " + id);
         }
+
+        return sight;
     }
 
 }
